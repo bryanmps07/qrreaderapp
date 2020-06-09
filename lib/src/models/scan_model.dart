4c1674b10885985dde29a798432f76f265b663b5
Bryan <bryan.m.p.s_07@live.com>
2020-06-09 17:04:02 -0400

    ultimo push hasta mover al punto central en el mapa

--- lib/src/models/scan_model.dart
+++ lib/src/models/scan_model.dart
@@ -1,3 +1,5 @@
+import 'package:latlong/latlong.dart';
+
 class ScanModel {
    
     int id;
@@ -28,8 +30,13 @@ class ScanModel {
         "valor" : valor,
     };
 
-    getLatLng() {
+    LatLng getLatLng() {
 
-      //final lalo = valor.substring(4);
+      // geo:40.754408249552085,-73.87410536250003
+      final lalo = valor.substring(4).split(',');
+      final lat = double.parse( lalo[0] );
+      final lng = double.parse( lalo[1] );
+
+      return LatLng( lat, lng );
     }
 }
--- lib/src/pages/mapa_page.dart
+++ lib/src/pages/mapa_page.dart
@@ -4,32 +4,82 @@ import 'package:flutter_map/flutter_map.dart';
 
 import 'package:qrreaderapp/src/models/scan_model.dart';
 
+
 class MapaPage extends StatelessWidget {
+
+  final map = new MapController();
+
   @override
   Widget build(BuildContext context) {
 
-    final ScanModel scan = ModalRoute.of(context).settings.arguments;
+    final ScanModel scan = ModalRoute.of(context).settings.arguments; 
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Coordenadas QR'),
-        actions: <Widget>[ 
+        actions: <Widget>[
           IconButton(
-            icon: Icon( Icons.my_location), 
-            onPressed: (){}
-          ) 
+            icon: Icon(Icons.my_location),
+            onPressed: (){
+              map.move(scan.getLatLng(), 15);
+            },
+          )
         ],
       ),
-      //body: _crearFlutterMap(scan),
+      body: _crearFlutterMap(scan)
     );
   }
 
   Widget _crearFlutterMap( ScanModel scan ) {
 
     return FlutterMap(
+      mapController: map,
       options: MapOptions(
-        //center: LatLng(),
+        center: scan.getLatLng(),
+        zoom: 15
       ),
+      layers: [
+        _crearMapa(),
+        _crearMarcadores( scan )
+      ],
     );
+
   }
+
+  _crearMapa() {
+
+    return TileLayerOptions(
+        urlTemplate: 'https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}',
+        additionalOptions: {
+        'accessToken':'pk.eyJ1IjoiYnJ5YW5tcHMwNyIsImEiOiJja2I2eWEyeXMwMGkyMnRucXpqaXBobTJuIn0.u2cxv6LYE9Q65OIHjDAgmg',
+        'id': 'mapbox/streets-v11' 
+        //streets-v11, outdoors-v11, light-v10, satellite-v9, satellite-streets-v11, dark-v10
+        }
+);
+
+
+  }
+
+  _crearMarcadores( ScanModel scan ) {
+
+
+    return MarkerLayerOptions(
+      markers: <Marker>[
+        Marker(
+          width: 120.0,
+          height: 120.0,
+          point: scan.getLatLng(),
+          builder: ( context ) => Container(
+            child: Icon( 
+              Icons.location_on,
+              size:  70.0,
+              color: Theme.of(context).primaryColor,
+            ),
+          )
+        )
+      ]
+    );
+
+  }
+
 }
